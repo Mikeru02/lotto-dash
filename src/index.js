a61f7f7fd50374f41376067e7fc34a5a4f52519b
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-02-25 19:17:03 +0800

    :rocket: Test socket communication

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import express from "express";
 import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
-import { dirname } from "node:path";
+import { dirname,join } from "node:path";
 import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server); 
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+    methods: ["GET", "POST"]
+  }
+}); 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.use(express.static(join(__dirname, "public")));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname, "public", "index.html");
+  res.sendFile(join(__dirname, "public", "index.html"));
 });
+
+io.on("connection", (socket) => {
+  console.log(`User connected at ${socket.id}`);
+
+  socket.on("chat", (msg) => {
+    io.emit("chat", msg);
+  })
+})
 
 server.listen(process.env.PORT, () => {
   console.log(`This app is running on port: ${process.env.PORT}`);

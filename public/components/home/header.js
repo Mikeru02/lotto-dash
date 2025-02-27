d741eb2d936f67680be8a5893622a59076e1e36f
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-02-28 07:13:03 +0800

    :heavy_check_mark: Integration of vite

--- public/components/home/header.js
+++ public/components/home/header.js
@@ -1,13 +1,15 @@
-import "./component.css";
+import styles from "./component.module.css";
 
 export default function Header(root){
   root.innerHTML = `
     <div>
       <h1>Lotto Dash</h1>
     </div>
-    <div class="login-btns">
+    <div class="${styles['login-btns']}">
       <a>Login</a>
       <a>Sign Up</a>
     </div>
-  `
+  `;
+
+  root.className = styles['header']
 }
--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@ import SPA from "./core/spa.js";
 import PageNotFound from "./pages/pageNotFound.js";
 import Home from "./pages/home.js";
 
+import "./styles/common.css";
+
 const app = new SPA ({
   root: document.getElementById("app"),
   defaultRoute: PageNotFound

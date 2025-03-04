d89affc5d3fbe8d42e7dc9669f452e125d2cd4f3
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-03-04 16:42:11 +0800

    :wrench: Fixes

--- public/layouts/default.js
+++ public/layouts/default.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
 export default function Layout(root) {
-    root.innerHTML = `
-        <header id="header"></header>
-        <main id="main"></main>
-        <footer id="footer"></footer>
-    `
-  
-    return {
-      header: document.getElementById('header'),
-      main: document.getElementById('main'),
-      footer: document.getElementById('footer')
-    }
-  }
-=======
-export default function Layout(root){
   root.innerHTML = `
     <header id="header"></header>
     <main id="main"></main>
     <footer id="footer"></footer>
   `
+
   return {
-    header: document.getElementById("header"),
-    main: document.getElementById("main"),
-    footer: document.getElementById("footer")
+    header: document.getElementById('header'),
+    main: document.getElementById('main'),
+    footer: document.getElementById('footer')
   }
 }
->>>>>>> 9e7a71cdb21d5995713f18cd21b666f996947892

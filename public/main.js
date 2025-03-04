17f1e9d2c8913707166a014cc8a25d791c6e876d
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-03-04 17:20:10 +0800

    :rocket: Added start page

--- public/main.js
+++ public/main.js
@@ -3,6 +3,8 @@ import PageNotFound from "./pages/pageNotFound";
 // TODO: Add pages here!
 import Landing from "./pages/landing.js";
 import Home from "./pages/home.js";
+import Profile from "./pages/profile.js";
+import Start from "./pages/start.js";
 
 // Uncomment this to implement styling
 //import "./styles/common.css";

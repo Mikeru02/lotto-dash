6c47ef41a646cd7bf812edd8baf449d8c5f6a4db
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2025-03-04 16:40:04 +0800

    :wrench: Fixed

--- public/main.js
+++ public/main.js
@@ -7,12 +7,6 @@ import Home from "./pages/home.js";
 // Uncomment this to implement styling
 //import "./styles/common.css";
 
-
-import SPA from "./core/spa.js";
-import PageNotFound from "./pages/pageNotFound.js";
-import Home from "./pages/home.js";
-import Landing from "./pages/landing.js";
-
 const app = new SPA ({
   root: document.getElementById("app"),
   defaultRoute: PageNotFound
